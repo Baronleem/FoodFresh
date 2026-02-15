914e5112ccc5658b1715b278d013c053778a5270
Liam Gliklich <132465661+Baronleem@users.noreply.github.com>
2026-02-15 10:34:07 -0500

    created food component and food json file

---
+++ src/app/food/food.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import FOOD_DATA from '../json/food.json';
+
+@Component({
+  selector: 'app-food',
+  imports: [],
+  templateUrl: './food.html',
+  styleUrl: './food.css',
+})
+export class Food {}
